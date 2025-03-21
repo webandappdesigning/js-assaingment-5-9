709ffb074802ec8b22461667f3501756f90de2e2
webandappdesigning <ayeshakhiyani08@gmail.com>
2025-03-21 02:31:05 -0700

    commit 1

---
+++ js/chapter 6-9.js
@@ -0,0 +1,116 @@
+// *-----Chapter 6-9 Math Expression Start-----*
+// ==== Question 1 =====
+
+// let a = +prompt('Input a number');
+// document.write('Result ' + '<br>');
+// document.write('The value of a is : ' + a + '<br>');
+// document.write("..............................................." + "<br>");
+// document.write("<br>");
+
+// document.write('The value of ++a is : ' + ++a + '<br>');
+// document.write('Now the value of a is : ' + a);
+// document.write("<br>");
+// document.write("<br>");
+
+// document.write('The value of a++ is :' + a++ + '<br>');
+// document.write('Now the value of a is :' + a);
+// document.write("<br>");
+// document.write("<br>");
+
+// document.write('The value of --a is :' + --a + '<br>');
+// document.write('Now the value of a is :' + a);
+// document.write("<br>");
+// document.write("<br>");
+
+// document.write('The value of a-- is :' + a-- + '<br>');
+// document.write('Now the value of a is :' + a);
+
+// ==== Question 2 =====
+
+// let a=2;
+// let b=1;
+// document.write('The value of a is :'+ a+'<br>');
+// document.write('The value of b is :'+ b+'<br>');
+// var result=  --a - --b + ++b + b--;
+// document.write('The result is '+result+'<br>'+'<br>');
+
+// ==== Question 3 =====
+
+// let user = prompt("Enter your name :");
+// alert("Hello! "+ user);
+
+// ==== Question 4 =====
+
+// let tableNo=+prompt('Enter a number');
+// if(tableNo==0){
+//     for(let i=1;i<=10;i++){document.write(5+ ' X '+i+"="+5*i+'<br>' )}
+    
+// }
+// else{
+//     for(let i=1;i<=10;i++) {document.write(tableNo+ ' X '+i+"="+tableNo*i+'<br>' )}
+// }
+
+// ==== Question 5 =====
+
+// document.write('<h3>'+'Marksheet'+'</h3>')
+
+// let sub1= prompt('Enter first subject name');
+// let sub2= prompt('Enter second subject name');
+// let sub3= prompt('Enter third subject name');
+
+// let totalmarks=300;
+
+// let obtainedSub1=+prompt('Enter your first subject marks')
+
+// let obtainedSub2=+prompt('Enter your second subject marks')
+// let obtainedSub3=+prompt('Enter your third subject marks')
+
+// let obtainedMarks=obtainedSub1+obtainedSub2+obtainedSub3;
+// let percentage=(obtainedMarks/totalmarks)*100;
+// percentage=percentage.toFixed(2);
+// let percentage1=(obtainedSub1/100)*100;
+// percentage1=percentage1.toFixed(2);
+// let percentage2=(obtainedSub2/100)*100;
+// percentage2=percentage2.toFixed(2);
+// let percentage3=(obtainedSub3/100)*100;
+// percentage3=percentage3.toFixed(2);
+
+// document.write('<Table>')
+// document.write('<tr>');
+// document.write('<th>'+'Subjects '+'</th>')
+// document.write('<th>'+' Total'+'</th>')
+// document.write('<th>'+' Obtained Marks '+'</th>')
+// document.write('<th>'+' Percentage '+'</th>')
+// document.write('</tr>');
+
+// document.write('<tr>');
+// document.write('<td>'+sub1+'</td>')
+// document.write('<td>'+100+'</td>')
+// document.write('<td>'+obtainedSub1+'</td>')
+// document.write('<td>'+percentage1+'%'+'</td>')
+// document.write('</tr>');
+
+
+// document.write('<tr>');
+// document.write('<td>'+sub2+'</td>')
+// document.write('<td>'+100+'</td>')
+// document.write('<td>'+obtainedSub2+'</td>')
+// document.write('<td>'+percentage2+'%'+'</td>')
+// document.write('</tr>');
+
+// document.write('<tr>');
+// document.write('<td>'+sub3+'</td>')
+// document.write('<td>'+100+'</td>')
+// document.write('<td>'+obtainedSub3+'</td>')
+// document.write('<td>'+percentage3+'%'+'</td>')
+// document.write('</tr>');
+
+// document.write("<tr>");
+// document.write("<th>" + "</th>");
+// document.write("<th>"+totalmarks+"</th>");
+// document.write("<th>"+obtainedMarks+"</th>");
+// document.write("<th>"+percentage+"%"+"</th>");
+// document.write("</tr>");
+// document.write("</Table>")
+
+// *-----Chapter 6-9 Math Expression End-----*
